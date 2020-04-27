8957ce6fe8900d1e3dfc56045c10457edeb8d055
MitziYolotzin <mitziulter@gmail.com>
2020-04-26 19:05:20 -0500

    create schema mongoose

---
+++ models/course.js
@@ -0,0 +1,8 @@
+//colecciones > tablas
+//documentos > filas
+
+const mongoose = require('mongoose');
+
+const courseSchema = new mongoose.Schema({
+
+});
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); //ODM
 
 mongoose.connect('mongodb://localhost/graphql_db_course', { useNewUrlParser: true, useUnifiedTopology: true })
 
